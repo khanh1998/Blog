819a12fcb630a6337733db5a30064c41acaf528d
= <=>
2019-06-01 15:31:12 +0700

    add welcome message

--- services/BlogAPI/app/routes/user.js
+++ services/BlogAPI/app/routes/user.js
@@ -3,6 +3,9 @@ import config from '../../config/index';
 import permit from '../../config/authorization';
 
 export const configUserRoute = (app, passport) => {
+  app.get('/', (req, res) => {
+    res.status(200).end("Welcome to BlogAPI\n For more infomation, access https://github.com/khanh1998/Blog");
+  });
   app.post('/api/v1/signup', signup);
   app.get('/api/v1/user/:username', getUser);
   app.put('/api/v1/user/', passport.authenticate('jwt', config.session), permit('reader', 'writer', 'admin'), updateUser);
